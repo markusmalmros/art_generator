59f91a8dc9492dba939075414f3617370e13025f
Markus Malmros <markusmalmros@Markuss-Macbook.local>
2018-10-31 14:34:16 +0100

    Added prints to art scraper

--- src/data_download/genre-scraper.py
+++ src/data_download/genre-scraper.py
@@ -55,7 +55,7 @@ def get_painting_list(count, typep, searchword):
         regex = r'https?://uploads[0-9]+[^/\s]+/\S+\.jpg'
         url_list = re.findall(regex, str(soup.html()))
         count += len(url_list)
-        print(url_list)
+        print(count)
         return url_list
     except Exception as e:
         print('failed to scrape %s'%url, e)
