caac09bfdf1334b9ed9ad224153d4dc72d61a06f
markusmalmros <markus.malmros@gmail.com>
2018-11-17 22:22:00 +0100

    New artist list output

--- src/src/art_scraping.py
+++ src/src/art_scraping.py
@@ -69,7 +69,7 @@ def download_artwork(url, artist_name, out_dir):
     img_name = img_soup['title']
     img_link = img_soup['src']
 
-    out_path = out_dir + '/' + artist_name + '/' + img_name.replace("/", "_").encode('utf-8').strip() + '.jpg'
+    out_path = (out_dir + '/' + artist_name + '/' + img_name.replace("/", "_") + '.jpg').encode('utf-8')
     urllib.request.urlretrieve(img_link, out_path)
 
 def download_one_artists_artworks():
